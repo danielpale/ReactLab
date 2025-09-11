034d435c3a01dbd9c48ba73eb18014aa20b69591
danielpale <danielpp79@gmail.com>
2025-09-10 18:53:33 -0600

    fix: Corrección de errores de estilos.

--- project-01-todo-app/src/App.jsx
+++ project-01-todo-app/src/App.jsx
@@ -1,13 +1,14 @@
 // import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>Todo List App</h1>
+      <main>
+        <h1>Todo List App</h1>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
