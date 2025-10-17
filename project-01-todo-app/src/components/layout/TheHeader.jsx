eaabb8fcd430f9074cf58bacc0a5db2b0a87dd44
danielpale <danielpp79@gmail.com>
2025-10-17 17:00:34 -0600

    feat: Se terminó el componente TheHeader

--- project-01-todo-app/src/components/layout/TheHeader.jsx
+++ project-01-todo-app/src/components/layout/TheHeader.jsx
@@ -1,11 +1,23 @@
 import classes from "./TheHeader.module.css";
 import logo from "../../assets/images/logo-light.svg";
 
+import BaseSection from "../base/BaseSection";
+
 export default function TheHeader() {
   return (
     <header className={classes["the-header"]}>
-      <img src={logo} alt="logo" />
-      <h6>Todo App</h6>
+      <BaseSection
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "32px",
+          paddingBlock: "0",
+        }}
+        fullHeight
+      >
+        <img src={logo} alt="logo" />
+        <h6 className="ff-text">Todo App</h6>
+      </BaseSection>
     </header>
   );
 }
